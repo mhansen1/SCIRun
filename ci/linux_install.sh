ab41445def00619a9e4b5693552867369177b603
Dan White <dwhite@sci.utah.edu>
2014-11-04 16:14:42 -0700

    GCC attempt

--- ci/linux_install.sh
+++ ci/linux_install.sh
@@ -3,6 +3,7 @@
 sudo apt-get install -qq cmake
 sudo apt-get install -qq libosmesa6-dev
 sudo apt-get install -qq libqt4-dev qt4-qmake
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9
 echo "$CXX"
-if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
-if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
+if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.9; fi
+if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi
